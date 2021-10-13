3c5f396f7b5663688ec8d6bd57970cd9421027d7
joaopsc <joao.pedro@qualyteam.com.br>
2021-10-12 21:10:20 -0300

    create user repository

---
+++ src/users/users.mongo.repository.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { IUserRepository } from './Iusers.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UserMongoRepository implements IUserRepository {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = new this.userModel(createUserDto);
+    const userSaved = await user.save();
+    return userSaved;
+  }
+
+  async findAll() {
+    const users = await this.userModel.find();
+    return users;
+  }
+
+  async findOne(id: string) {
+    const user = await this.userModel.findById(id);
+    return user;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const user = await this.userModel.findByIdAndUpdate(
+      {
+        _id: id,
+      },
+      {
+        $set: updateUserDto,
+      },
+      {
+        new: true,
+      },
+    );
+    return user;
+  }
+
+  async remove(id: string) {
+    const idUser = await this.userModel
+      .remove({
+        _id: id,
+      })
+      .exec();
+    return idUser;
+  }
+}
