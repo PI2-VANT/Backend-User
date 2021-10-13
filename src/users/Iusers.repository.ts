dc167104aed87b377239371066eeda645471870c
joaopsc <joao.pedro@qualyteam.com.br>
2021-10-12 21:10:01 -0300

    create user repository interface

---
+++ src/users/Iusers.repository.ts
@@ -0,0 +1,11 @@
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+export interface IUserRepository {
+  create(createUserDto: CreateUserDto): Promise<User>;
+  findAll(): Promise<User[] | undefined>;
+  findOne(id: string): Promise<User | undefined>;
+  update(id: string, updateUserDto: UpdateUserDto): Promise<User | undefined>;
+  remove(id: string): Promise<string>;
+}
