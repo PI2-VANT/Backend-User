cd8c06e28d53a813355e94f1f6ef29d06a7e91c3
Lucas Gomes <lucasg@lacunasoftware.com>
2021-10-14 04:34:10 -0300

    Refactored docker, added to get environment variables and removed default tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
     MongooseModule.forRoot(
-      'mongodb+srv://backendUser:userdb123@user.faxt8.mongodb.net/test',
+      `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@db_backend_user:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     ),
     UsersModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
